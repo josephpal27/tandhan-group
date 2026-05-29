30105489db9c3c262ecf3239b2a2c0d60153bfc7
Joseph Pal <josephpal27@gmail.com>
2026-05-29 18:20:51 +0530

    Images Updated

--- src/components/home/GlobalPresence.jsx
+++ src/components/home/GlobalPresence.jsx
@@ -26,7 +26,8 @@ const GlobalPresence = () => {
     }, []);
 
     return (
-        <section className="mt-[5rem] mb-[1rem] px-0 relative">
+        <section className="mt-[5rem] sm:mt-[3rem] lg:mt-[4rem] xl:mt-[4.5rem] 2xl:mt-[5rem] 
+        mb-[1rem] px-0 relative">
 
             {/* Map Image */}
             <div>
@@ -42,7 +43,8 @@ const GlobalPresence = () => {
             </div>
 
             {/* Content Box */}
-            <div className="absolute w-full h-full left-0 top-0 px-[1rem] sm:px-[5%] lg:px-[7%] py-[4.5rem]">
+            <div className="absolute w-full h-full left-0 top-0 px-[1rem] sm:px-[5%] lg:px-[7%] 
+            py-[4.5rem] sm:py-[3rem] lg:py-[3.5rem] xl:py-[4rem] 2xl:py-[4.5rem]">
                 <div className="w-[30%] h-full bg-white shadow-[rgba(0,0,0,0.30)_0px_3px_8px]">
                     {/* Top */}
                     <div className="relative">
@@ -50,14 +52,18 @@ const GlobalPresence = () => {
                         {/* Layer */}
                         <div className="absolute top-0 left-0 w-full h-full bg-primary/30 flex items-end">
                             <div className="w-[90%] flex justify-between">
-                                <div className="bg-white w-[80%] py-[2rem] px-[1rem]">
-                                    <span className="text-[1.4rem] font-semibold block">
+                                <div className="
+                                    bg-white w-[80%] 
+                                    py-[2rem] sm:py-[1.5rem] lg:py-[1.6rem] xl:py-[1.8rem] 2xl:py-[2rem]
+                                    px-[1rem]
+                                ">
+                                    <span className="text-[1.4rem] sm:text-[1.3rem] lg:text-[1.2rem] xl:text-[1.3rem] 2xl:text-[1.4rem] font-semibold block">
                                         {activeLocation.country}
                                     </span>
                                 </div>
                                 <div onClick={handleNext} className="
                                     bg-secondary hover:bg-primary text-white w-[20%] flex justify-center items-center 
-                                    text-[3rem]
+                                    text-[3rem] sm:text-[2rem] lg:text-[2.8rem] xl:text-[2.9rem] 2xl:text-[3rem]
                                     cursor-pointer transition select-none
                                 ">
                                     ›
@@ -71,18 +77,18 @@ const GlobalPresence = () => {
                         {activeLocation.brands.map((item, index) => (
                             <div
                                 key={index}
-                                className={`py-[0.8rem] ${index !== 0 ? "border-t border-gray-200" : ""}`}
+                                className={`py-[0.8rem] sm:py-[0.9rem] lg:py-[0.6rem] xl:py-[0.7rem] 2xl:py-[0.8rem] ${index !== 0 ? "border-t border-gray-200" : ""}`}
                             >
-                                <span className="block text-[1.3rem] font-semibold">
+                                <span className="block text-[1.3rem] sm:text-[1.3rem] lg:text-[1.1rem] xl:text-[1.2rem] 2xl:text-[1.3rem] font-semibold">
                                     {item.brand}
                                 </span>
 
                                 <div className="flex justify-between mt-[0.5rem]">
                                     <div className="w-[5%]">
-                                        <SlLocationPin className="mt-[0.3rem]" />
+                                        <SlLocationPin className="mt-[0.3rem] sm:mt-[0.2rem] lg:mt-[0.25rem] xl:mt-[0.29rem] 2xl:mt-[0.3rem]" />
                                     </div>
                                     <div className="w-[93%]">
-                                        <p className="text-[1rem]">
+                                        <p className="text-[1rem] sm:text-[1.1rem] lg:text-[0.8rem] xl:text-[0.9rem] 2xl:text-[1rem]">
                                             {item.address}
                                         </p>
                                     </div>
--- src/components/home/LatestNews.jsx
+++ src/components/home/LatestNews.jsx
@@ -24,7 +24,7 @@ const latestNewsData = [
 
 const LatestNews = () => {
     return (
-        <section className="pt-[4.5rem]">
+        <section className="pt-[4.5rem] sm:pt-[3rem] lg:pt-[3.5rem] xl:pt-[4rem] 2xl:pt-[4.5rem]">
             <div className="flex flex-col items-center text-center">
                 <h6>
                     Latest News
@@ -35,7 +35,7 @@ const LatestNews = () => {
             </div>
 
             {/* Row */}
-            <div className="mt-[2.5rem] flex justify-between flex-wrap">
+            <div className="mt-[2.5rem] sm:mt-[2rem] lg:mt-[2.1rem] xl:mt-[2.3rem] 2xl:mt-[2.5rem] flex justify-between flex-wrap">
                 {latestNewsData.map((item) => {
                     return(
                         <div key={item.id} className="
@@ -45,26 +45,30 @@ const LatestNews = () => {
                                 <img src={item.image} alt={item.title} loading="lazy" className="w-full" />
                                 <div className="
                                     absolute top-0 left-0 bg-secondary p-[1rem]
-                                    text-[1.3rem] font-semibold
+                                    text-[1.3rem] sm:text-[1.3rem] lg:text-[1.1rem] xl:text-[1.2rem] 2xl:text-[1.3rem] font-semibold
                                 ">
                                     0{item.id}
                                 </div>
                             </div>
                             <div className="
                                 text-white
-                                px-[1.5rem]
-                                pt-[1.5rem]
-                                pb-[6rem]
+                                px-[1.5rem] sm:px-[1.3rem] lg:px-[1.2rem] xl:px-[1.4rem] 2xl:px-[1.5rem]
+                                pt-[1.5rem] sm:pt-[1.3rem] lg:pt-[1.2rem] xl:pt-[1.4rem] 2xl:pt-[1.5rem]
+                                pb-[6rem] sm:pb-[4.5rem] lg:pb-[5.6rem] xl:pb-[5.8rem] 2xl:pb-[6rem]
                             ">
-                                <span className="text-[1.5rem] font-semibold">
+                                <span className="text-[1.5rem] sm:text-[1.3rem] lg:text-[1.3rem] xl:text-[1.4rem] 2xl:text-[1.5rem] font-semibold">
                                     {item.title}
                                 </span>
-                                <p className="mt-[0.7rem] text-[1rem]">
+                                <p className="mt-[0.7rem] text-[1rem] sm:text-[1.1rem] lg:text-[0.8rem] xl:text-[0.9rem] 2xl:text-[1rem]">
                                     {item.desc}
                                 </p>
                                 <a href="/" className="
-                                    absolute bottom-0 right-0 w-[70px] aspect-square bg-secondary
-                                    flex justify-center items-center text-[3rem] text-black
+                                    absolute bottom-0 right-0 
+                                    w-[70px] sm:w-[50px] lg:w-[60px] xl:w-[65px] 2xl:w-[70px] 
+                                    aspect-square bg-secondary
+                                    flex justify-center items-center 
+                                    text-[3rem] sm:text-[2rem] lg:text-[2.6rem] xl:text-[2.8rem] 2xl:text-[3rem]
+                                    text-black
                                     transition duration-300 hover:bg-[#ced430]
                                 ">
                                     ›
