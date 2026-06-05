29b886ab3ef7089b180ddfb400f3292fe5b2c901
Joseph Pal <josephpal27@gmail.com>
2026-06-05 14:04:52 +0530

    Home Page Mobile View Added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -58,45 +58,45 @@ const Footer = () => {
         ">
             {/* Top */}
             <div className="
-                pr-[1rem] sm:pr-[5%] lg:pr-[7%]
-                pl-[1rem] sm:pl-[5%] lg:pl-0
+                pr-0 lg:pr-[7%]
                 flex justify-between flex-wrap
             ">
                 {/* Left */}
-                <div className="w-[35%] relative">
+                <div className="w-full lg:w-[35%] relative">
                     <img src="/images/footer-bg.avif" alt="Footer Background" loading="lazy" className="w-full aspect-[1/1] object-cover" />
                     <div className="
-                        absolute top-0 right-0 w-[78%] h-full bg-secondary/90 text-black
+                        absolute bottom-0 right-[15%] lg:right-0 w-[85%] lg:w-[78%] h-[86%] lg:h-full bg-secondary/90 text-black
                         p-[1rem] sm:p-[1.5rem] lg:p-[1.6rem] xl:p-[1.8rem] 2xl:p-[2rem]
                     ">
                         <img
                             src="/images/logo/logo.png"
                             alt="Tandhan Group"
                             loading="lazy"
-                            className="w-[190px] sm:w-[150px] lg:w-[150px] xl:w-[170px] 2xl:w-[190px]"
+                            className="w-[140px] sm:w-[145px] lg:w-[150px] xl:w-[170px] 2xl:w-[190px]"
                         />
-                        <p className="font-[500] mt-[1.5rem]">
+                        <p className="font-[500] mt-[1.2rem] lg:mt-[1.5rem]">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae suscipit error enim assumenda pariatur, delectus consequatur eos! Id, sed corrupti.
                         </p>
-                        <p className="font-[500] mt-[1rem]">
+                        <p className="font-[500] mt-[0.5rem] lg:mt-[1rem]">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae suscipit error enim assumenda pariatur.
                         </p>
                     </div>
                 </div>
 
                 {/* Right */}
-                <div className="w-[60%] pt-[4rem] sm:pt-[3rem] lg:pt-[3rem] xl:pt-[3.5rem] 2xl:pt-[4rem] flex justify-between flex-wrap">
+                <div className="w-full lg:w-[60%] pt-[1.5rem] sm:pt-[2rem] lg:pt-[3rem] xl:pt-[3.5rem] 2xl:pt-[4rem] flex justify-between flex-wrap px-[1rem] lg:px-0">
 
                     {/* Quick Links & Sectors */}
                     {footerData.map((item, index) => {
                         return (
                             <div key={index} className="
-                                w-[32%] text-white
+                                w-full lg:w-[32%] text-white
+                                mb-[1rem] lg:mb-0
                             ">
-                                <span className="text-[1.5rem] sm:text-[1.5rem] lg:text-[1.15rem] xl:text-[1.35rem] 2xl:text-[1.5rem] font-semibold">
+                                <span className="text-[1.3rem] sm:text-[1.4rem] lg:text-[1.15rem] xl:text-[1.35rem] 2xl:text-[1.5rem] font-semibold">
                                     {item.title}
                                 </span>
-                                <ul className="mt-[1.2rem] sm:mt-[1.3rem] lg:mt-[1rem] xl:mt-[1.1rem] 2xl:mt-[1.2rem]">
+                                <ul className="mt-[0.7rem] sm:mt-[0.8rem] lg:mt-[1rem] xl:mt-[1.1rem] 2xl:mt-[1.2rem]">
                                     {item.links.map((link, index) => {
                                         return (
                                             <li key={index} className="mt-[0.5rem]">
@@ -115,11 +115,11 @@ const Footer = () => {
                     })}
 
                     {/* Get In Touch */}
-                    <div className="w-[32%] text-white">
-                        <span className="text-[1.5rem] sm:text-[1.5rem] lg:text-[1.15rem] xl:text-[1.35rem] 2xl:text-[1.5rem] font-semibold">
+                    <div className="w-full lg:w-[32%] text-white">
+                        <span className="text-[1.3rem] sm:text-[1.4rem] lg:text-[1.15rem] xl:text-[1.35rem] 2xl:text-[1.5rem] font-semibold">
                             Get In Touch
                         </span>
-                        <ul className="mt-[1.2rem] sm:mt-[1.3rem] lg:mt-[1rem] xl:mt-[1.1rem] 2xl:mt-[1.2rem]">
+                        <ul className="mt-[0.7rem] sm:mt-[0.8rem] lg:mt-[1rem] xl:mt-[1.1rem] 2xl:mt-[1.2rem]">
                             {contactLinks.map((item, index) => (
                                 <li key={index} className="mt-[0.5rem]">
                                     <a href={item.url} target={item.target}
@@ -136,8 +136,8 @@ const Footer = () => {
             </div>
 
             {/* Copyright */}
-            <div className="py-[1rem] sm:py-[1rem] lg:py-[0.8rem] xl:py-[0.9rem] 2xl:py-[1rem] px-[1rem] bg-[#05115f]">
-                <p className="text-center text-[1rem] sm:text-[1rem] lg:text-[0.8rem] xl:text-[0.9rem] 2xl:text-[1rem] text-white">
+            <div className="py-[1rem] sm:py-[1rem] lg:py-[0.8rem] xl:py-[0.9rem] 2xl:py-[1rem] px-[1rem] bg-[#05115f] mt-[1.5rem] lg:mt-0">
+                <p className="text-center text-[0.9rem] sm:text-[1rem] lg:text-[0.8rem] xl:text-[0.9rem] 2xl:text-[1rem] text-white">
                     Copyright © 2026 | Tandhan Group | All Rights Reserved
                 </p>
             </div>
