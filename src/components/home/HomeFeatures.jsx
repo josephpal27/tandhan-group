f0f53181cc07374ac1adb4463c032f0af8784fa3
Joseph Pal <josephpal27@gmail.com>
2026-06-05 12:17:44 +0530

    Features Mobile View Added

--- src/components/home/HomeFeatures.jsx
+++ src/components/home/HomeFeatures.jsx
@@ -29,17 +29,17 @@ const featuresData = [
 const HomeFeatures = () => {
     return (
         <section className="flex justify-between flex-wrap 
-        mt-[-2rem] sm:mt-[-2.5rem] lg:mt-[-3rem] xl:mt-[-4rem] 2xl:mt-[-5rem] relative">
+        mt-[1.5rem] lg:mt-[-3rem] xl:mt-[-4rem] 2xl:mt-[-5rem] relative">
             {featuresData.map((feature) => {
                 return (
-                    <div key={feature.id} className="w-[25%] bg-primary p-[2rem] border-b-[10px] border-secondary group">
+                    <div key={feature.id} className="w-full lg:w-[25%] bg-primary p-[1.2rem] lg:p-[2rem] border-b-[10px] border-secondary mb-[1rem] lg:mb-0 group">
                         <img 
                             src={feature.icon} 
                             alt={feature.title} 
                             loading="lazy" 
                             className="w-[20%]" 
                         />
-                        <span className="text-[1.5rem] sm:text-[1.6rem] lg:text-[1.6rem] xl:text-[1.7rem] 2xl:text-[1.8rem] text-white font-semibold block mt-[0.8rem]">
+                        <span className="text-[1.4rem] sm:text-[1.6rem] lg:text-[1.6rem] xl:text-[1.7rem] 2xl:text-[1.8rem] text-white font-semibold block mt-[0.8rem]">
                             {feature.title}
                         </span>
                         <p className="text-gray-300 mt-[0.5rem] text-[1rem] sm:text-[1.1rem] lg:text-[0.8rem] xl:text-[0.9rem] 2xl:text-[1rem]">
