548971bed5a47c6480a4a72fc39364cb0aba3b25
Joseph Pal <josephpal27@gmail.com>
2026-06-09 12:48:23 +0530

    Mission Vision Init

---
+++ src/components/about/MissionVision.jsx
@@ -0,0 +1,53 @@
+const missionVisionData = [
+    {
+        id: 1,
+        title: "Mission",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, commodi quaerat unde, vero incidunt accusantium odit fugit maiores itaque eveniet.",
+        image: "/images/mission.png",
+    },
+    {
+        id: 2,
+        title: "Vision",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, commodi quaerat unde, vero incidunt accusantium odit fugit maiores itaque eveniet.",
+        image: "/images/vision.png",
+    },
+]
+
+const MissionVision = () => {
+    return (
+        <section className="px-0 pt-[3.5rem] sm:pt-[3.7rem] lg:pt-[4rem] xl:pt-[4.5rem] 2xl:pt-[5rem]">
+            {missionVisionData.map((item) => {
+                return (
+                    <div key={item.id} className={`
+                        flex justify-between flex-wrap
+                        ${item.id % 2 === 0 ? "flex-row-reverse mt-[2rem]" : ""}
+                    `}>
+                        <div className="w-[60%] overflow-hidden relative">
+                            <img src={item.image} alt={item.title} loading="lazy" className="w-full" />
+                            <div className={`
+                                absolute w-full h-full top-0 left-0 flex justify-between
+                            `}>
+                                <div className="w-[48%]">
+
+                                </div>
+                                <div className="w-[48%]">
+                                    <span>
+                                        {item.title}
+                                    </span>
+                                    <p>
+                                        {item.desc}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-[35%]">
+
+                        </div>
+                    </div>
+                )
+            })}
+        </section>
+    )
+}
+
+export default MissionVision
