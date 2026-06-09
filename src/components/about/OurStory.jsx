666356d00f7aec9dd726409696f155f7a7e95cdc
Joseph Pal <josephpal27@gmail.com>
2026-06-09 11:44:37 +0530

    Our Story Added

--- src/components/about/OurStory.jsx
+++ src/components/about/OurStory.jsx
@@ -1,7 +1,25 @@
 
 const OurStory = () => {
     return (
-        <section>
+        <section className="pt-[3.5rem] sm:pt-[3.7rem] lg:pt-[4rem] xl:pt-[4.5rem] 2xl:pt-[5rem] flex justify-between flex-wrap items-center">
+
+            {/* Slider */}
+            <div className="w-[48%]">
+                
+            </div>
+
+            {/* Content */}
+            <div className="w-[48%]">
+                <h2>
+                    Our Story
+                </h2>
+                <p className="mt-[0.7rem] lg:mt-[1.5rem]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, commodi quaerat unde, vero incidunt accusantium odit fugit maiores itaque eveniet, dolor amet recusandae aut numquam voluptatem ab tempore quae sit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quaerat quae eveniet vitae modi aspernatur adipisci necessitatibus recusandae harum assumenda.
+                </p>
+                <p className="mt-[0.3rem] lg:mt-[1rem]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, commodi quaerat unde, vero incidunt accusantium odit fugit maiores itaque eveniet, dolor amet recusandae aut numquam voluptatem ab tempore quae sit.
+                </p>
+            </div>
 
         </section>
     )
