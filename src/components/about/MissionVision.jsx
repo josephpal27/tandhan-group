bf35ad15baee70762dc5b5de57efdfbd54b99085
Joseph Pal <josephpal27@gmail.com>
2026-06-10 15:11:20 +0530

    Mission Vision Updated

--- src/components/about/MissionVision.jsx
+++ src/components/about/MissionVision.jsx
@@ -25,24 +25,36 @@ const MissionVision = () => {
                         <div className="w-[60%] overflow-hidden relative">
                             <img src={item.image} alt={item.title} loading="lazy" className="w-full" />
                             <div className={`
-                                absolute w-full h-full top-0 left-0 flex justify-between
+                                absolute w-full h-full top-0 left-0 flex justify-between text-black
                                 p-[5rem]
+                                ${item.id % 2 === 0 ? "flex-row-reverse items-end text-white" : ""}
                             `}>
                                 <div className="w-[45%]">
 
                                 </div>
                                 <div className="w-[52%]">
-                                    <span>
+                                    <span className="text-[2.5rem] font-semibold">
                                         {item.title}
                                     </span>
-                                    <p>
+                                    <p className="mt-[1rem]">
                                         {item.desc}
                                     </p>
                                 </div>
                             </div>
                         </div>
-                        <div className="w-[35%]">
-
+                        <div className={`
+                            w-[35%] flex justify-start relative
+                            ${item.id % 2 === 0 ? "justify-end" : ""}
+                        `}>
+                            <img 
+                                src="/images/artboard.png" 
+                                alt="Artboard" 
+                                loading="lazy" 
+                                className={`
+                                    w-[85%] filter invert absolute
+                                    top-[-4rem]
+                                `} 
+                            />
                         </div>
                     </div>
                 )
