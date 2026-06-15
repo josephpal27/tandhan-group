62a1e791f8623220685246825887ebffc87774eb
Joseph Pal <josephpal27@gmail.com>
2026-06-15 16:10:38 +0530

    Protective Solutions Page Init

---
+++ src/app/protective-solutions/page.js
@@ -0,0 +1,18 @@
+import HeroBanner from "@/components/HeroBanner";
+
+export const metadata = {
+  title: "Protective Solutions - Tandhan Group",
+  description: "",
+};
+
+export default function ProtectiveSolutions() {
+  return (
+    <>
+      <HeroBanner
+        image="/images/banners/about-banner.avif"
+        title={<>Lorem Ipsum <br /> Dolor Sit Amet</>}
+        desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio numquam nihil voluptatibus laboriosam reiciendis veniam deserunt, quas voluptatem molestias deleniti vitae exercitationem."
+      />
+    </>
+  );
+}
