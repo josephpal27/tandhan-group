f7b78bf1323404eab2423ab29fc1c110e0a102fb
Joseph Pal <josephpal27@gmail.com>
2026-06-16 18:00:56 +0530

    In The News Added

--- src/components/InTheNews.jsx
+++ src/components/InTheNews.jsx
@@ -2,8 +2,33 @@
 
 const InTheNews = () => {
     return (
-        <section>
-            
+        <section className="pt-[2rem] sm:pt-[2.2rem] lg:pt-[4rem] xl:pt-[4.5rem] 2xl:pt-[5rem] flex justify-between flex-wrap">
+
+            {/* Left */}
+            <div className="w-[60%]">
+                <div>
+                    01
+                </div>
+                <h6>
+                    In The <br /> News
+                </h6>
+
+                {/* Slider */}
+                <div>
+
+                </div>
+            </div>
+
+            {/* Right */}
+            <div className="w-[35%]">
+                <span>
+                    Lorem Ipsum
+                </span>
+                <p className="mt-[0.7rem] lg:mt-[1.2rem]">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus, quas.
+                </p>
+            </div>
+
         </section>
     )
 }
