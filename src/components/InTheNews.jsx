93f75f40368230f40ed1201275fb5cc91251075b
Joseph Pal <josephpal27@gmail.com>
2026-06-17 12:31:23 +0530

    In The News Updated

--- src/components/InTheNews.jsx
+++ src/components/InTheNews.jsx
@@ -1,6 +1,65 @@
 "use client";
 
+import { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+const newsData = [
+    {
+        id: 1,
+        number: "01",
+        img: "/images/sectors/protective-solutions/news/1.png",
+        title: "Lorem Ipsum 1",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident.",
+        link: "#",
+    },
+    {
+        id: 2,
+        number: "02",
+        img: "/images/sectors/protective-solutions/news/1.png",
+        title: "Lorem Ipsum 2",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident.",
+        link: "#",
+    },
+    {
+        id: 3,
+        number: "03",
+        img: "/images/sectors/protective-solutions/news/1.png",
+        title: "Lorem Ipsum 3",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident.",
+        link: "#",
+    },
+    {
+        id: 4,
+        number: "04",
+        img: "/images/sectors/protective-solutions/news/1.png",
+        title: "Lorem Ipsum 4",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident.",
+        link: "#",
+    },
+    {
+        id: 5,
+        number: "05",
+        img: "/images/sectors/protective-solutions/news/1.png",
+        title: "Lorem Ipsum 5",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident.",
+        link: "#",
+    },
+];
+
 const InTheNews = () => {
+
+    const swiperRef = useRef(null);
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const activeData = newsData[activeIndex];
+
+    const handleNext = () => {
+        if (swiperRef.current) {
+            swiperRef.current.slideNext();
+        }
+    };
+
     return (
         <section className="pt-[2rem] sm:pt-[2.2rem] lg:pt-[4rem] xl:pt-[4.5rem] 2xl:pt-[5rem] flex justify-between flex-wrap">
 
@@ -15,14 +74,43 @@ const InTheNews = () => {
 
                 {/* Slider */}
                 <div>
-
+                    <Swiper
+                        dir="rtl"
+                        slidesPerView={2.6}
+                        spaceBetween={16}
+                        loop={true}
+                        onSwiper={(swiper) => (swiperRef.current = swiper)}
+                        onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+                    >
+                        {newsData.map((item, index) => {
+                            const isActive = index === activeIndex;
+                            return (
+                                <SwiperSlide key={item.id}>
+                                    <div
+                                        dir="ltr"
+                                        className="overflow-hidden h-[80vh] flex items-end cursor-pointer"
+                                        onClick={() => swiperRef.current?.slideToLoop(index)}
+                                    >
+                                        <img
+                                            src={item.img}
+                                            alt={item.title}
+                                            className={`
+                                            w-full object-cover transition-all duration-500 ease-in-out 
+                                            ${isActive ? "h-[550px]" : "h-[280px]"}
+                                        `}
+                                        />
+                                    </div>
+                                </SwiperSlide>
+                            );
+                        })}
+                    </Swiper>
                 </div>
             </div>
 
             {/* Right */}
             <div className="w-[35%]">
                 <span>
-                    Lorem Ipsum
+                    {activeData.title}
                 </span>
                 <p className="mt-[0.7rem] lg:mt-[1.2rem]">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus, quas.
@@ -34,7 +122,7 @@ const InTheNews = () => {
                     h-[35px] sm:h-[40px] lg:h-[50px] xl:h-[55px] 2xl:h-[60px]
                     flex items-center justify-center bg-secondary hover:bg-primary hover:text-white transition 
                     text-[1.8rem] sm:text-[1.8rem] lg:text-[1.8rem] xl:text-[2rem] 2xl:text-[2.2rem]
-                " id="news-next">
+                " id="news-next" onClick={handleNext}>
                     ›
                 </button>
             </div>
