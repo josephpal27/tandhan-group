b55cd88e379b277a9cf1c479dbad82a71a12250f
Joseph Pal <josephpal27@gmail.com>
2026-06-17 11:31:02 +0530

    Sectors updated

--- src/components/InTheNews.jsx
+++ src/components/InTheNews.jsx
@@ -27,6 +27,16 @@ const InTheNews = () => {
                 <p className="mt-[0.7rem] lg:mt-[1.2rem]">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus, quas.
                 </p>
+
+                {/* Next Btn */}
+                <button className="
+                    w-[35px] sm:w-[40px] lg:w-[40px] xl:w-[45px] 2xl:w-[50px]
+                    h-[35px] sm:h-[40px] lg:h-[50px] xl:h-[55px] 2xl:h-[60px]
+                    flex items-center justify-center bg-secondary hover:bg-primary hover:text-white transition 
+                    text-[1.8rem] sm:text-[1.8rem] lg:text-[1.8rem] xl:text-[2rem] 2xl:text-[2.2rem]
+                " id="news-next">
+                    ›
+                </button>
             </div>
 
         </section>
