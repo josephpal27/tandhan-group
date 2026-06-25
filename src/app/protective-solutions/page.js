daa751673f10bb66a51944d0ca315340d0f80e23
Joseph Pal <josephpal27@gmail.com>
2026-06-25 15:50:43 +0530

    Sector Overview Init

--- src/app/protective-solutions/page.js
+++ src/app/protective-solutions/page.js
@@ -2,6 +2,7 @@ import InTheNews from "@/components/InTheNews";
 import BrandOverview from "@/components/protective-solutions/BrandOverview";
 import PolyplastGallery from "@/components/protective-solutions/PolyplastGallery";
 import PolyplastProducts from "@/components/protective-solutions/PolyplastProducts";
+import SectorOverview from "@/components/protective-solutions/SectorOverview";
 import SectorsBanner from "@/components/SectorsBanner";
 import SectorsCTA from "@/components/SectorsCTA";
 import SectorsFAQ from "@/components/SectorsFAQ";
@@ -19,6 +20,7 @@ export default function ProtectiveSolutions() {
         title={<>Lorem Ipsum <br /> Dolor Sit Amet</>}
         desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio numquam nihil voluptatibus laboriosam reiciendis veniam deserunt, quas voluptatem molestias deleniti vitae exercitationem."
       />
+      <SectorOverview />
       <BrandOverview />
       <PolyplastProducts />
       <PolyplastGallery />
---
+++ src/components/protective-solutions/SectorOverview.jsx
@@ -0,0 +1,30 @@
+
+const SectorOverview = () => {
+    return (
+        <section>
+
+            {/* Left */}
+            <div>
+                <h2>
+                    Sector Overview
+                </h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto minima adipisicing tenetur asperiores nihil repellat pariatur explicabo tenetur exercitationem molestiae ducimus fugiat.
+                </p>
+            </div>
+
+            {/* Center */}
+            <div>
+
+            </div>
+
+            {/* Right */}
+            <div>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default SectorOverview
