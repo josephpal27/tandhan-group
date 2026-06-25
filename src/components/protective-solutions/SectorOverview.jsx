0ce35dc3d40f555e74f36086856c4aa4a6f113fa
Joseph Pal <josephpal27@gmail.com>
2026-06-25 16:32:51 +0530

    Sectors Responsiveness added

--- src/components/protective-solutions/SectorOverview.jsx
+++ src/components/protective-solutions/SectorOverview.jsx
@@ -11,7 +11,7 @@ const SectorOverview = () => {
         <section className="flex justify-between flex-wrap relative">
 
             {/* Left */}
-            <div className="w-[30%] pt-[5rem] relative z-50">
+            <div className="w-[30%] pt-[5rem] sm:pt-[3rem] lg:pt-[4rem] xl:pt-[4.5rem] 2xl:pt-[5rem] relative z-50">
                 <h2>
                     Sector Overview
                 </h2>
@@ -20,12 +20,12 @@ const SectorOverview = () => {
                 </p>
                 <p className={`
                     mt-[0.7rem] lg:mt-[1rem] w-[80%] overflow-hidden transition-all duration-300
-                    ${active ? "max-h-40 opacity-100" : "max-h-0 opacity-0"}
+                    ${active ? "max-h-40 opacity-100 mb-[1rem]" : "max-h-0 opacity-0"}
                 `}>
                     Lorem ipsum dolor sit amet consectetur elit. Architecto tenetur asperiores nihil repellat adipisicing.
                 </p>
                 <button className="
-                    mt-[2.5rem] bg-secondary hover:bg-primary text-white font-semibold transition
+                    mt-[1.5rem] bg-secondary hover:bg-primary text-white font-semibold transition
                     py-[0.5rem] px-[1.5rem]
                 " onClick={() => setActive(!active)}>
                     {active ? "Read Less" : "Read More"}
@@ -43,14 +43,14 @@ const SectorOverview = () => {
             </div>
 
             {/* Right */}
-            <div className="w-[30%] flex items-end pb-[3rem] relative z-50">
+            <div className="w-[30%] flex items-end pb-[3rem] sm:pb-[1.5rem] lg:pb-[2rem] xl:pb-[2.5rem] 2xl:pb-[3rem] relative z-50">
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto minima adipisicing tenetur asperiores nihil repellat pariatur explicabo tenetur exercitationem molestiae ducimus fugiat.
                 </p>
             </div>
 
             {/* Truck Image */}
-            <div className="absolute top-0 left-0 w-full h-full flex justify-center items-end pb-[5rem]">
+            <div className="absolute top-0 left-0 w-full h-full flex justify-center items-end pb-[5rem] sm:pb-[3rem] lg:pb-[3.7rem] xl:pb-[4.5rem] 2xl:pb-[5rem]">
                 <img 
                     src="/images/sectors/protective-solutions/truck.png" 
                     alt="Truck" 
