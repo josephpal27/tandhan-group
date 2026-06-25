6b5604090f9ae9cd12fcb05b6802b5cc88dfed60
Joseph Pal <josephpal27@gmail.com>
2026-06-25 16:03:20 +0530

    Sector Overview Updated

--- src/components/protective-solutions/SectorOverview.jsx
+++ src/components/protective-solutions/SectorOverview.jsx
@@ -1,26 +1,39 @@
 
 const SectorOverview = () => {
     return (
-        <section>
+        <section className="flex justify-between flex-wrap">
 
             {/* Left */}
-            <div>
+            <div className="w-[30%] pt-[5rem]">
                 <h2>
                     Sector Overview
                 </h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto minima adipisicing tenetur asperiores nihil repellat pariatur explicabo tenetur exercitationem molestiae ducimus fugiat.
+                <p className="mt-[0.7rem] lg:mt-[1.2rem]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto tenetur asperiores nihil repellat.
                 </p>
+                <button className="
+                    mt-[2.5rem] bg-secondary hover:bg-primary text-white font-semibold transition
+                    py-[0.5rem] px-[1.5rem]
+                ">
+                    Read More
+                </button>
             </div>
 
             {/* Center */}
-            <div>
-
+            <div className="w-[32%]">
+                <img 
+                    src="/images/sectors/protective-solutions/overview-bg.png" 
+                    alt="Sector Overview" 
+                    loading="lazy" 
+                    className="w-full" 
+                />
             </div>
 
             {/* Right */}
-            <div>
-
+            <div className="w-[30%] flex items-end pb-[3rem]">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto minima adipisicing tenetur asperiores nihil repellat pariatur explicabo tenetur exercitationem molestiae ducimus fugiat.
+                </p>
             </div>
 
         </section>
