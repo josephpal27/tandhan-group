15cf49d14329675558d32f25cc8cf4318e764e4a
Joseph Pal <josephpal27@gmail.com>
2026-07-03 13:54:01 +0530

    Our Values Responsiveness Added

--- src/components/about/Founder.jsx
+++ src/components/about/Founder.jsx
@@ -12,20 +12,25 @@ const Founder = () => {
         ">
             {/* Image */}
             <div className="w-[35%]">
-                <img 
-                    src="/images/founder-demo.avif" 
-                    alt="Founder" 
-                    loading="lazy" 
-                    className="w-full" 
+                <img
+                    src="/images/founder-demo.avif"
+                    alt="Founder"
+                    loading="lazy"
+                    className="w-full"
                 />
             </div>
 
             {/* Content */}
             <div className="w-[60%] bg-secondary/10 flex flex-col justify-center p-[4rem] relative overflow-hidden">
                 <img src="/images/quote.avif" alt="Quote" loading="lazy" className="w-[25%] mb-[3rem]" />
-                <h5>
+                <motion.h5
+                    variants={headingVariant}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                >
                     Founder's Message
-                </h5>
+                </motion.h5>
                 <p className="mt-[0.7rem] lg:mt-[1.2rem]">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda fugiat adipisicing elit tenetur dolorum commodi voluptatem debitis itaque facere adipisicing elit accusamus numquam nam. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 </p>
@@ -42,13 +47,13 @@ const Founder = () => {
                 ">
                     Read More
                 </Link>
-                
+
                 {/* Artboard */}
-                <img 
-                    src="/images/artboard.png" 
-                    alt="Artboard" 
-                    loading="lazy" 
-                    className="w-[60%] filter invert absolute top-[-39%] right-[-15%]" 
+                <img
+                    src="/images/artboard.png"
+                    alt="Artboard"
+                    loading="lazy"
+                    className="w-[60%] filter invert absolute top-[-39%] right-[-15%]"
                 />
             </div>
         </section>
