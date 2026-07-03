2588b11a8006627b843958cb0e7623b0694fff31
Joseph Pal <josephpal27@gmail.com>
2026-07-03 12:10:09 +0530

    Founder Image Compressed

--- src/components/about/Founder.jsx
+++ src/components/about/Founder.jsx
@@ -2,90 +2,46 @@
 
 import { motion } from "framer-motion";
 import { headingVariant } from "@/utils/animations";
+import Link from "next/link";
 
 const Founder = () => {
     return (
         <section className="
             pt-[2rem] sm:pt-[2.2rem] lg:pt-[4rem] xl:pt-[4.5rem] 2xl:pt-[5rem]
-            flex justify-between flex-wrap
+            flex justify-between items-center flex-wrap
         ">
-            <div className="w-[20%] hidden lg:block">
-                <motion.div
-                    initial={{ opacity: 0, y: 60 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    viewport={{ once: true }}
-                    transition={{ duration: 0.6, ease: "easeOut" }}
-                >
-                    <img
-                        src="/images/qoute.png"
-                        alt="Qoute Left"
-                        loading="lazy"
-                        className="
-                        w-[230px] sm:w-[160px] lg:w-[185px] xl:w-[205px] 2xl:w-[230px]
-                        mt-[1rem] brightness-0 invert-0
-                    "
-                    />
-                </motion.div>
+            {/* Image */}
+            <div className="w-[35%]">
+                <img 
+                    src="/images/founder-demo.avif" 
+                    alt="Founder" 
+                    loading="lazy" 
+                    className="w-full" 
+                />
             </div>
-            <div className="w-full lg:w-[79%]">
-                {/* Top */}
-                <div>
-                    <motion.h4
-                        variants={headingVariant}
-                        initial="hidden"
-                        whileInView="visible"
-                        viewport={{ once: true }}
-                    >
-                        Founder's Message
-                    </motion.h4>
-                    <p className="mt-[0.7rem] lg:mt-[1.2rem]">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam officiis magnam eum quas delectus, nemo accusamus quam, ipsam id totam in adipisci inventore et a, velit reprehenderit? Deserunt, necessitatibus possimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam officiis magnam eum quas delectus, nemo accusamus quam, ipsam id totam in adipisci inventore et a, velit reprehenderit? Deserunt, necessitatibus possimus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    </p>
-                </div>
 
-                {/* Bottom */}
-                <div className="
-                    mt-[1.5rem] sm:mt-[1.7rem] lg:mt-[2.4rem] xl:mt-[2.6rem] 2xl:mt-[3.2rem]
-                    flex justify-between
+            {/* Content */}
+            <div className="w-[55%]">
+                <img src="/images/quote.avif" alt="Quote" loading="lazy" className="w-[25%] mb-[3rem]" />
+                <h5>
+                    Founder's Message
+                </h5>
+                <p className="mt-[0.7rem] lg:mt-[1.2rem]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda fugiat adipisicing elit tenetur dolorum commodi voluptatem debitis itaque facere adipisicing elit accusamus numquam nam. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+                <div className="flex gap-[0.8rem] items-center mt-[1rem]">
+                    <div className="w-[45px] h-[2px] bg-primary"></div>
+                    <span className="text-[1.7rem] font-semibold">
+                        John Doe
+                    </span>
+                </div>
+                <Link href="/" className="
+                    mt-[2rem] block w-max bg-white hover:bg-primary text-primary hover:text-white font-semibold transition
+                    border-primary border-[3px]
+                    py-[0.4rem] px-[1.4rem]
                 ">
-                    <div className="w-[30%] hidden lg:block">
-                        <motion.div
-                            initial={{ opacity: 0, y: -60 }}
-                            whileInView={{ opacity: 1, y: 0 }}
-                            viewport={{ once: true }}
-                            transition={{ duration: 0.6, ease: "easeOut" }}
-                        >
-                            <img
-                                src="/images/qoute.png"
-                                alt="Qoute Left"
-                                loading="lazy"
-                                className="
-                                    w-[230px] sm:w-[160px] lg:w-[185px] xl:w-[205px] 2xl:w-[230px]
-                                    rotate-[180deg] brightness-0 invert-0
-                                "
-                            />
-                        </motion.div>
-                    </div>
-                    <div className="w-full lg:w-[70%] relative group">
-                        <img
-                            src="/images/founder-demo.png"
-                            alt="Founder"
-                            loading="lazy"
-                            className="w-full h-full"
-                        />
-                        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-end p-[1rem] lg:p-[2rem] text-white">
-                            <span className="text-[1.4rem] sm:text-[1.5rem] lg:text-[1.4rem] xl:text-[1.6rem] 2xl:text-[1.8rem] font-semibold">
-                                John Doe
-                            </span>
-                            <p>
-                                - Founder
-                            </p>
-                        </div>
-                        <div className="absolute w-[55%] lg:w-[40%] h-[10px] lg:h-[14px] left-[22.5%] lg:left-[30%] bottom-[-5px] lg:bottom-[-7px] bg-secondary group-hover:w-[70%] group-hover:left-[15%] transition-all duration-300">
-
-                        </div>
-                    </div>
-                </div>
+                    Read More
+                </Link>
             </div>
         </section>
     )
