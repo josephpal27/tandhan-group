35ecfb6c6332e9f5485e581971a2f97534274812
Joseph Pal <josephpal27@gmail.com>
2026-07-08 17:34:59 +0530

    CTA Updated

---
+++ src/data/sectorsCtaData.js
@@ -0,0 +1,19 @@
+export const sectorsCtaData = {
+    protective: {
+        cta: {
+            title: "Lorem ipsum dolor",
+            desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus, quas.",
+            image: "/images/sectors/protective-solutions/cta.avif",
+            url: "/",
+        },
+    },
+
+    energy: {
+        cta: {
+            title: "Lorem ipsum dolor",
+            desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus, quas.",
+            image: "/images/sectors/protective-solutions/cta.avif",
+            url: "/",
+        },
+    },
+}
