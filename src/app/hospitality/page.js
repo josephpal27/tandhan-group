b96c9c910955dc7f36ef788f784c457dbcbdfb26
Joseph Pal <josephpal27@gmail.com>
2026-07-13 15:45:40 +0530

    Hospitality Page Init

---
+++ src/app/hospitality/page.js
@@ -0,0 +1,51 @@
+import InTheNews from "@/components/sectors/InTheNews";
+import SectorsGallery from "@/components/sectors/SectorsGallery";
+import SectorsProducts from "@/components/sectors/SectorsProducts";
+import SectorsBanner from "@/components/sectors/SectorsBanner";
+import SectorsCTA from "@/components/sectors/SectorsCTA";
+import SectorsFAQ from "@/components/sectors/SectorsFAQ";
+import EnergySectorOverview from "@/components/energy-solutions/EnergySectorOverview";
+import EnergyBrandOverview from "@/components/energy-solutions/EnergyBrandOverview";
+
+import { sectorsFaqData } from "@/data/sectorsFaqData";
+import { inTheNewsData } from "@/data/inTheNewsData";
+import { sectorsProductsData } from "@/data/sectorsProductsData";
+import { sectorsCtaData } from "@/data/sectorsCtaData";
+import { sectorsGalleryData } from "@/data/sectorsGalleryData";
+
+export const metadata = {
+  title: "Hospitality - Tandhan Group",
+  description: "",
+};
+
+export default function EnergySolutions() {
+  return (
+    <>
+      <SectorsBanner
+        image="/images/banners/energy-solutions-banner.avif"
+        title="Hospitality"
+        desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio numquam nihil voluptatibus laboriosam reiciendis veniam deserunt, quas voluptatem molestias deleniti vitae exercitationem."
+        business="NX Hotel"
+      />
+      <EnergySectorOverview />
+      <EnergyBrandOverview />
+      {/* <SectorsProducts 
+        products={sectorsProductsData.energy}
+      />
+      <SectorsGallery 
+        gallery={sectorsGalleryData.energy}
+      />
+      <SectorsFAQ
+        description={sectorsFaqData.energy.description}
+        faqs={sectorsFaqData.energy.faqs}
+        image={sectorsFaqData.energy.image}
+      />
+      <InTheNews
+        news={inTheNewsData.energy.news}
+      />
+      <SectorsCTA 
+        cta={sectorsCtaData.energy.cta}
+      /> */}
+    </>
+  );
+}
