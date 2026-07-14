91339ede7fcdd3bf4b1524966a3c737280f7e36d
Joseph Pal <josephpal27@gmail.com>
2026-07-14 12:08:04 +0530

    News Data Updated

--- src/data/inTheNewsData.js
+++ src/data/inTheNewsData.js
@@ -62,4 +62,33 @@ export const inTheNewsData = {
             },
         ],
     },
+
+    hospitality: {
+        news: [
+            {
+                id: 1,
+                img: "/images/sectors/hospitality/news/1.avif",
+                title: "Lorem Ipsum 1",
+                description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident.",
+            },
+            {
+                id: 2,
+                img: "/images/sectors/hospitality/news/2.avif",
+                title: "Lorem Ipsum 2",
+                description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident.",
+            },
+            {
+                id: 3,
+                img: "/images/sectors/hospitality/news/1.avif",
+                title: "Lorem Ipsum 3",
+                description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident.",
+            },
+            {
+                id: 4,
+                img: "/images/sectors/hospitality/news/2.avif",
+                title: "Lorem Ipsum 4",
+                description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident.",
+            },
+        ],
+    },
 }
