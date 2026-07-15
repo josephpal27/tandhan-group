83d1d2edfa7730d4b097f15a02bc6f73875b4730
Joseph Pal <josephpal27@gmail.com>
2026-07-15 15:48:11 +0530

    Textiles Page Init

---
+++ src/app/textiles/page.js
@@ -0,0 +1,51 @@
+import InTheNews from "@/components/sectors/InTheNews";
+import SectorsGallery from "@/components/sectors/SectorsGallery";
+import SectorsProducts from "@/components/sectors/SectorsProducts";
+import SectorsBanner from "@/components/sectors/SectorsBanner";
+import SectorsCTA from "@/components/sectors/SectorsCTA";
+import SectorsFAQ from "@/components/sectors/SectorsFAQ";
+import HospitalitySectorOverview from "@/components/hospitality/HospitalitySectorOverview";
+import HospitalityBrandOverview from "@/components/hospitality/HospitalityBrandOverview";
+
+import { sectorsFaqData } from "@/data/sectorsFaqData";
+import { inTheNewsData } from "@/data/inTheNewsData";
+import { sectorsProductsData } from "@/data/sectorsProductsData";
+import { sectorsCtaData } from "@/data/sectorsCtaData";
+import { sectorsGalleryData } from "@/data/sectorsGalleryData";
+
+export const metadata = {
+  title: "Textiles - Tandhan Group",
+  description: "",
+};
+
+export default function EnergySolutions() {
+  return (
+    <>
+      <SectorsBanner
+        image="/images/banners/textiles-banner.avif"
+        title="Textiles"
+        desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio numquam nihil voluptatibus laboriosam reiciendis veniam deserunt, quas voluptatem molestias deleniti vitae exercitationem."
+        business="Tandhan Denim"
+      />
+      <HospitalitySectorOverview />
+      <HospitalityBrandOverview />
+      <SectorsProducts 
+        products={sectorsProductsData.hospitality}
+      />
+      <SectorsGallery 
+        gallery={sectorsGalleryData.hospitality}
+      />
+      <SectorsFAQ
+        description={sectorsFaqData.hospitality.description}
+        faqs={sectorsFaqData.hospitality.faqs}
+        image={sectorsFaqData.hospitality.image}
+      />
+      <InTheNews
+        news={inTheNewsData.hospitality.news}
+      />
+      <SectorsCTA 
+        cta={sectorsCtaData.hospitality.cta}
+      />
+    </>
+  );
+}
