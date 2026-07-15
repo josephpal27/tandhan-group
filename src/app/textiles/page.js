08dc18ced8cdb516cf5c6817bae253839dad2826
Joseph Pal <josephpal27@gmail.com>
2026-07-15 18:21:13 +0530

    Textiles Page added

--- src/app/textiles/page.js
+++ src/app/textiles/page.js
@@ -1,6 +1,6 @@
 import InTheNews from "@/components/sectors/InTheNews";
 import SectorsGallery from "@/components/sectors/SectorsGallery";
-import SectorsProducts from "@/components/sectors/SectorsProducts";
+// import SectorsProducts from "@/components/sectors/SectorsProducts";
 import SectorsBanner from "@/components/sectors/SectorsBanner";
 import SectorsCTA from "@/components/sectors/SectorsCTA";
 import SectorsFAQ from "@/components/sectors/SectorsFAQ";
@@ -9,7 +9,7 @@ import TextilesBrandOverview from "@/components/textiles/TextilesBrandOverview";
 
 import { sectorsFaqData } from "@/data/sectorsFaqData";
 import { inTheNewsData } from "@/data/inTheNewsData";
-import { sectorsProductsData } from "@/data/sectorsProductsData";
+// import { sectorsProductsData } from "@/data/sectorsProductsData";
 import { sectorsCtaData } from "@/data/sectorsCtaData";
 import { sectorsGalleryData } from "@/data/sectorsGalleryData";
 
@@ -29,11 +29,11 @@ export default function EnergySolutions() {
       />
       <TextilesSectorOverview />
       <TextilesBrandOverview />
-      <SectorsProducts
+      {/* <SectorsProducts
         products={sectorsProductsData.hospitality}
-      />
+      /> */}
       <SectorsGallery
-        gallery={sectorsGalleryData.hospitality}
+        gallery={sectorsGalleryData.textiles}
       />
       <SectorsFAQ
         description={sectorsFaqData.hospitality.description}
