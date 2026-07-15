20d3fdf9af59182b2d8e2b477bd7b19a9992d1c1
Joseph Pal <josephpal27@gmail.com>
2026-07-15 12:50:15 +0530

    Sector Products Mobile View Added

--- src/components/protective-solutions/PolyplastBrandOverview.jsx
+++ src/components/protective-solutions/PolyplastBrandOverview.jsx
@@ -7,12 +7,12 @@ import { headingVariant } from "@/utils/animations";
 const PolyplastBrandOverview = () => {
     return (
         <section className="
-            mt-[5rem] sm:mt-[3rem] lg:mt-[4rem] xl:mt-[4.5rem] 2xl:mt-[5rem]
-            mb-[5rem] sm:mb-[3rem] lg:mb-[4rem] xl:mb-[4.5rem] 2xl:mb-[5rem]
+            mt-[0.5rem] sm:mt-[3rem] lg:mt-[4rem] xl:mt-[4.5rem] 2xl:mt-[5rem]
+            mb-[3rem] sm:mb-[3.5rem] lg:mb-[4rem] xl:mb-[4.5rem] 2xl:mb-[5rem]
             px-0 bg-primary
         ">
-            <div className="flex justify-between flex-wrap pl-[1rem] sm:pl-[5%] lg:pl-[7%] text-white">
-                <div className="w-[43%] flex flex-col justify-center">
+            <div className="flex justify-between flex-wrap pl-[1rem] sm:pl-[5%] lg:pl-[7%] pr-[1rem] lg:pr-0 text-white">
+                <div className="w-full lg:w-[43%] flex flex-col justify-center pt-[2rem] lg:pt-0">
                     <motion.h3
                         variants={headingVariant}
                         initial="hidden"
@@ -25,14 +25,14 @@ const PolyplastBrandOverview = () => {
                         Tandhan Polyplast is one of Eastern India's leading manufacturers of high-performance polymer solutions, serving customers across India and international markets. Backed by advanced manufacturing facilities, stringent quality standards, and a growing export footprint, the company produces durable, application-specific solutions that meet the evolving needs of agriculture, infrastructure, and industry.
                     </p>
                     <Link href="/" className="
-                        mt-[2.5rem] bg-primary hover:bg-white w-max text-white hover:text-primary font-semibold transition
-                        border-white border-[3px]
+                        mt-[2rem] lg:mt-[2.5rem] bg-primary hover:bg-white w-max text-white hover:text-primary font-semibold transition
+                        border-white border-[2px] sm:border-[2.5px] lg:border-[2px] xl:border-[2.5px] 2xl:border-[3px]
                         py-[0.45rem] px-[1.4rem]
                     ">
                         Read More
                     </Link>
                 </div>
-                <div className="w-[54%] relative">
+                <div className="w-full lg:w-[54%] relative mt-[2.5rem] lg:mt-0 pb-[1rem] lg:pb-0">
                     <img src="/images/paper-roll.png" alt="Brand Overview" loading="lazy" className="w-full" />
                     <div className="w-full h-full absolute top-0 left-0 flex justify-center items-center bg-[#000000b8]">
                         <img src="/images/logo/tandhan-polyplast.png" alt="Tandhan Polyplast" loading="lazy" className="w-[40%] invert brightness-0" />
--- src/components/sectors/SectorsProducts.jsx
+++ src/components/sectors/SectorsProducts.jsx
@@ -13,8 +13,8 @@ const SectorsProducts = ({ products }) => {
 
             {/* Top */}
             <div className="relative">
-                <img src={products.background} alt="Products Background" loading="lazy" className="w-full" />
-                <div className="px-[1rem] sm:px-[5%] lg:px-[7%] absolute top-0 left-0 w-full h-full text-white pt-[5rem] sm:pt-[3rem] lg:pt-[4rem] xl:pt-[4.5rem] 2xl:pt-[5rem] bg-gradient-to-b from-primary/80 to-primary/40">
+                <img src={products.background} alt="Products Background" loading="lazy" className="w-full h-[200px] lg:h-auto object-cover" />
+                <div className="px-[1rem] sm:px-[5%] lg:px-[7%] absolute top-0 left-0 w-full h-full text-white pt-[1rem] sm:pt-[1.5rem] lg:pt-[4rem] xl:pt-[4.5rem] 2xl:pt-[5rem] bg-gradient-to-b from-primary/80 to-primary/40">
                     <motion.h4
                         variants={headingVariant}
                         initial="hidden"
@@ -23,14 +23,14 @@ const SectorsProducts = ({ products }) => {
                     >
                         {products.title}
                     </motion.h4>
-                    <p className="mt-[0.7rem] lg:mt-[1.2rem] w-[50%]">
+                    <p className="mt-[0.7rem] lg:mt-[1.2rem] w-full lg:w-[50%]">
                         {products.desc}
                     </p>
                 </div>
             </div>
 
             {/* Bottom */}
-            <div className="px-[1rem] sm:px-[5%] lg:px-[7%] mt-[-9rem] sm:mt-[-4rem] lg:mt-[-7rem] xl:mt-[-8rem] 2xl:mt-[-9rem]">
+            <div className="px-[1rem] sm:px-[5%] lg:px-[7%] mt-[1rem] sm:mt-[1.5rem] lg:mt-[-7rem] xl:mt-[-8rem] 2xl:mt-[-9rem]">
                 <Swiper
                     modules={[Navigation, Pagination]}
                     slidesPerView={2}
@@ -63,26 +63,26 @@ const SectorsProducts = ({ products }) => {
                         return (
                             <SwiperSlide key={index} className="relative group">
                                 <img src={product.image} alt={product.title} loading="lazy" className="
-                                    w-[70%] group-hover:scale-[1.05] transition duration-500
+                                    w-full lg:w-[70%] lg:group-hover:scale-[1.05] transition duration-500
                                 " />
                                 <div className="
-                                    absolute top-1/2 -translate-y-1/2
-                                    left-0 w-[65%] ml-[35%] pr-[5px]
+                                    relative lg:absolute top-0 lg:top-1/2 lg:-translate-y-1/2
+                                    left-0 w-full lg:w-[65%] ml-0 lg:ml-[35%] lg:pr-[5px]
                                     flex justify-end
                                     origin-right
                                     scale-x-100
-                                    group-hover:scale-x-0
-                                    group-hover:opacity-0
+                                    lg:group-hover:scale-x-0
+                                    lg:group-hover:opacity-0
                                     transition-all
                                     duration-500
                                     ease-in-out
                                 ">
                                     <div className="
-                                        bg-white p-[2rem] sm:p-[1.5rem] lg:p-[1.6rem] xl:p-[1.8rem] 2xl:p-[2rem] text-black 
-                                        min-h-[55%] shadow-[1px_1px_10px_rgba(0,0,0,0.3)]
+                                        bg-white p-0 lg:p-[1.6rem] xl:p-[1.8rem] 2xl:p-[2rem] text-black 
+                                        min-h-[55%] lg:shadow-[1px_1px_10px_rgba(0,0,0,0.3)]
                                     ">
                                         <span className="
-                                            text-[1.8rem] sm:text-[1.4rem] lg:text-[1.6rem] xl:text-[1.7rem] 2xl:text-[1.8rem] font-semibold
+                                            text-[1.5rem] sm:text-[1.6rem] lg:text-[1.6rem] xl:text-[1.7rem] 2xl:text-[1.8rem] font-semibold mt-[1rem] lg:mt-0 block
                                         ">
                                             {product.title}
                                         </span>
@@ -97,14 +97,14 @@ const SectorsProducts = ({ products }) => {
                 </Swiper>
 
                 {/* Pagination & Navigation */}
-                <div className="flex justify-center items-center py-[4rem] sm:py-[2rem] lg:py-[3.6rem] xl:py-[3.8rem] 2xl:py-[4rem] relative">
+                <div className="flex justify-center items-center py-[2rem] sm:py-[2.5rem] lg:py-[3.6rem] xl:py-[3.8rem] 2xl:py-[4rem] relative">
                     <div className="hidden lg:block">
                         <div id="team-pagination" className="flex items-center gap-[0.4rem]" />
                     </div>
                     <div className="flex items-center absolute right-0">
                         <button className="
-                            w-[35px] sm:w-[40px] lg:w-[40px] xl:w-[45px] 2xl:w-[50px]
-                            h-[35px] sm:h-[40px] lg:h-[50px] xl:h-[55px] 2xl:h-[60px]
+                            w-[40px] sm:w-[45px] lg:w-[40px] xl:w-[45px] 2xl:w-[50px]
+                            h-[40px] sm:h-[45px] lg:h-[50px] xl:h-[55px] 2xl:h-[60px]
                             flex items-center justify-center bg-white hover:bg-primary text-primary hover:text-white transition 
                             text-[1.8rem] sm:text-[1.8rem] lg:text-[1.8rem] xl:text-[2rem] 2xl:text-[2.2rem]
                             border-primary border-[2px] lg:border-[3px] border-r-[2px]
@@ -112,8 +112,8 @@ const SectorsProducts = ({ products }) => {
                             ‹
                         </button>
                         <button className="
-                            w-[35px] sm:w-[40px] lg:w-[40px] xl:w-[45px] 2xl:w-[50px]
-                            h-[35px] sm:h-[40px] lg:h-[50px] xl:h-[55px] 2xl:h-[60px]
+                            w-[40px] sm:w-[45px] lg:w-[40px] xl:w-[45px] 2xl:w-[50px]
+                            h-[40px] sm:h-[45px] lg:h-[50px] xl:h-[55px] 2xl:h-[60px]
                             flex items-center justify-center bg-white hover:bg-primary text-primary hover:text-white transition 
                             text-[1.8rem] sm:text-[1.8rem] lg:text-[1.8rem] xl:text-[2rem] 2xl:text-[2.2rem]
                             border-primary border-[2px] lg:border-[3px] border-l-[2px]
