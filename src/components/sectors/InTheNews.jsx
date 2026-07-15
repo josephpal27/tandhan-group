f8da0c2bcf243927a38a406042e29746ebb89e6f
Joseph Pal <josephpal27@gmail.com>
2026-07-15 13:34:29 +0530

    In The News Mobile View Added

--- src/components/sectors/InTheNews.jsx
+++ src/components/sectors/InTheNews.jsx
@@ -20,13 +20,13 @@ const InTheNews = ({ news }) => {
     };
 
     return (
-        <section className="pt-[2rem] sm:pt-[2.2rem] lg:pt-[1rem] xl:pt-[1.5rem] 2xl:pt-[2rem] flex justify-between flex-wrap">
+        <section className="pt-[3rem] sm:pt-[3.5rem] lg:pt-[1rem] xl:pt-[1.5rem] 2xl:pt-[2rem] flex justify-between flex-wrap">
 
             {/* Left */}
-            <div className="w-[60%]">
+            <div className="w-full lg:w-[60%]">
                 <div className="
-                    bg-primary text-[2.5rem] sm:text-[2rem] lg:text-[2.1rem] xl:text-[2.3rem] 2xl:text-[2.5rem] font-semibold w-[70px] sm:w-[50px] lg:w-[57px] xl:w-[62px] 2xl:w-[70px] flex justify-center items-center aspect-square
-                    mb-[2.5rem] sm:mb-[2rem] lg:mb-[2.1rem] xl:mb-[2.3rem] 2xl:mb-[2.5rem] text-white
+                    bg-primary text-[1.2rem] sm:text-[1.4rem] lg:text-[2.1rem] xl:text-[2.3rem] 2xl:text-[2.5rem] font-semibold w-[40px] sm:w-[45px] lg:w-[57px] xl:w-[62px] 2xl:w-[70px] flex justify-center items-center aspect-square
+                    mb-[1.5rem] sm:mb-[1.7rem] lg:mb-[2.1rem] xl:mb-[2.3rem] 2xl:mb-[2.5rem] text-white
                 ">
                     0{activeData.id}
                 </div>
@@ -40,13 +40,13 @@ const InTheNews = ({ news }) => {
                     In The <br /> News
                 </motion.h6>
                 <div className="relative z-50">
-                    <p className="mt-[0.7rem] lg:mt-[1rem] w-[50%]">
+                    <p className="mt-[0.7rem] lg:mt-[1rem] w-full lg:w-[50%] hidden lg:block">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem.
                     </p>
                 </div>
 
                 {/* Slider */}
-                <div className="mt-[-20rem] sm:mt-[-14rem] lg:mt-[-15.8rem] xl:mt-[-18rem] 2xl:mt-[-20rem]">
+                <div className="mt-[-32rem] sm:mt-[-26rem] lg:mt-[-15.8rem] xl:mt-[-18rem] 2xl:mt-[-20rem]">
                     <Swiper
                         dir="rtl"
                         slidesPerView={2.6}
@@ -55,6 +55,11 @@ const InTheNews = ({ news }) => {
                         onSwiper={(swiper) => (swiperRef.current = swiper)}
                         onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
                         className="!z-0"
+                        breakpoints={{
+                        0: { spaceBetween: 10, },
+                        640: { spaceBetween: 15, },
+                        991: { spaceBetween: 16, },
+                    }}
                     >
                         {news.map((item, index) => {
                             const isActive = index === activeIndex;
@@ -82,24 +87,25 @@ const InTheNews = ({ news }) => {
             </div>
 
             {/* Right */}
-            <div className="w-[35%] flex flex-col justify-end">
+            <div className="w-full lg:w-[35%] flex flex-col justify-end mt-[1rem] lg:mt-0">
                 <span className="
-                    text-[2rem] sm:text-[1.4rem] lg:text-[1.6rem] xl:text-[1.8rem] 2xl:text-[2rem] font-semibold
+                    text-[1.4rem] sm:text-[1.5rem] lg:text-[1.6rem] xl:text-[1.8rem] 2xl:text-[2rem] font-semibold
                 ">
                     {activeData.title}
                 </span>
-                <p className="mt-[0.7rem] lg:mt-[1.2rem]">
+                <p className="mt-[0.3rem] lg:mt-[1.2rem]">
                     {activeData.description}
                 </p>
 
                 {/* Next Btn */}
                 <button className="
-                    mt-[4rem] sm:mt-[2rem] lg:mt-[3.5rem] xl:mt-[3.8rem] 2xl:mt-[4rem]
-                    w-[35px] sm:w-[40px] lg:w-[40px] xl:w-[45px] 2xl:w-[50px]
-                    h-[35px] sm:h-[40px] lg:h-[50px] xl:h-[55px] 2xl:h-[60px]
+                    mt-[0.5rem] sm:mt-[1rem] lg:mt-[3.5rem] xl:mt-[3.8rem] 2xl:mt-[4rem]
+                    w-[45px] sm:w-[50px] lg:w-[40px] xl:w-[45px] 2xl:w-[50px]
+                    h-[45px] sm:h-[50px] lg:h-[50px] xl:h-[55px] 2xl:h-[60px]
                     flex items-center justify-center bg-white hover:bg-primary text-primary hover:text-white transition 
-                    text-[1.8rem] sm:text-[1.8rem] lg:text-[1.8rem] xl:text-[2rem] 2xl:text-[2.2rem]
+                    text-[2.1rem] sm:text-[2.5rem] lg:text-[1.8rem] xl:text-[2rem] 2xl:text-[2.2rem]
                     active:scale-[0.85] border-primary border-[2px] lg:border-[3px] border-r-[2px]
+                    ml-auto lg:ml-0
                 " id="news-next" onClick={handleNext}>
                     ›
                 </button>
