45fa91b92139329fb6701f1bbfd1638791dfd6be
Joseph Pal <josephpal27@gmail.com>
2026-07-16 18:24:04 +0530

    Mission Vision Updated

---
+++ src/data/initiativesData.js
@@ -0,0 +1,82 @@
+export const initiativesData = [
+    {
+        id: 1,
+        title: "Digital Learning Initiative",
+        posts: [
+            {
+                image: "/images/sustainability/initiatives/1.avif",
+                title: "Lorem Ipsum",
+                description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur officia numquam.",
+            },
+            {
+                image: "/images/sustainability/initiatives/2.avif",
+                title: "Lorem Ipsum",
+                description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur officia numquam.",
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: "Computer Training Programme for Underprivileged Students",
+        posts: [
+            {
+                image: "/images/sustainability/initiatives/1.avif",
+                title: "Lorem Ipsum",
+                description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur officia numquam.",
+            },
+            {
+                image: "/images/sustainability/initiatives/2.avif",
+                title: "Lorem Ipsum",
+                description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur officia numquam.",
+            },
+            {
+                image: "/images/sustainability/initiatives/1.avif",
+                title: "Lorem Ipsum",
+                description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur officia numquam.",
+            },
+        ]
+    },
+    {
+        id: 3,
+        title: "Blood Donation Camps",
+        posts: [
+            {
+                image: "/images/sustainability/initiatives/1.avif",
+                title: "Lorem Ipsum",
+                description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur officia numquam.",
+            },
+            {
+                image: "/images/sustainability/initiatives/2.avif",
+                title: "Lorem Ipsum",
+                description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur officia numquam.",
+            },
+        ]
+    },
+    {
+        id: 4,
+        title: "Eye Check-up and Cataract Surgery Camps",
+        posts: [
+            {
+                image: "/images/sustainability/initiatives/1.avif",
+                title: "Lorem Ipsum",
+                description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur officia numquam.",
+            },
+        ]
+    },
+    {
+        id: 5,
+        title: "Community Health & Wellness Programmes",
+        posts: [
+            {
+                image: "/images/sustainability/initiatives/1.avif",
+                title: "Lorem Ipsum",
+                description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur officia numquam.",
+            },
+            {
+                image: "/images/sustainability/initiatives/2.avif",
+                title: "Lorem Ipsum",
+                description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur officia numquam.",
+            },
+        ]
+    },
+]
