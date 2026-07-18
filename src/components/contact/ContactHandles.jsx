69e8460bc15b12e77923eae3adbbae8ec47c1e8e
Joseph Pal <josephpal27@gmail.com>
2026-07-18 16:23:50 +0530

    Contact Page Updated

---
+++ src/components/contact/ContactHandles.jsx
@@ -0,0 +1,52 @@
+
+import { FaFacebookF, FaXTwitter, FaInstagram, FaLinkedinIn, FaYoutube } from "react-icons/fa6";
+
+const handleIcons = [
+    {   id: 1, icon: <FaFacebookF /> },
+    {   id: 2, icon: <FaXTwitter /> },
+    {   id: 3, icon: <FaInstagram /> },
+    {   id: 4, icon: <FaLinkedinIn /> },
+    {   id: 5, icon: <FaYoutube /> },
+]
+
+const ContactHandles = () => {
+    return (
+        <section className="
+            pt-[2rem] sm:pt-[3rem] lg:pt-[4rem] xl:pt-[4.5rem] 2xl:pt-[5rem]
+            pb-[2rem] sm:pb-[3rem] lg:pb-[4.5rem] xl:pb-[5rem] 2xl:pb-[5.5rem]
+            flex flex-col items-center text-center
+        ">
+
+            <img 
+                src="/images/logo/logo.png" 
+                alt="Tandhan Group" 
+                loading="lazy" 
+                className="w-[250px]" 
+            />
+
+            <p className="mt-[0.7rem] lg:mt-[1rem] w-full lg:w-[60%]">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique consectetur amet voluptates amet saepe quo at optio beatae quod eaque quam totam aspernatur.
+            </p>
+
+            <div className="mt-[2rem] flex justify-center gap-[1rem]">
+                {handleIcons.map((item, index) => {
+                    return(
+                        <div key={index} className="
+                            w-[40px] 
+                            h-[40px] 
+                            text-[1.2rem]
+                            rounded-full bg-[#2d2d2d] hover:bg-primary text-white flex justify-center items-center transition duration-300
+                        ">
+                            <a href="#" target="_blank">
+                                {item.icon}
+                            </a>
+                        </div>
+                    )
+                })}
+            </div>
+
+        </section>
+    )
+}
+
+export default ContactHandles
---
+++ src/components/contact/OfficeLocation.jsx
@@ -0,0 +1,31 @@
+
+const OfficeLocation = () => {
+    return (
+        <section className="
+            pt-[2rem] sm:pt-[3rem] lg:pt-[4.5rem] xl:pt-[5rem] 2xl:pt-[5.5rem]
+            px-0
+        ">
+
+            {/* Head */}
+            <div className="px-[1rem] sm:px-[5%] lg:px-[7%]">
+                <h4>
+                    Head Office Address
+                </h4>
+                <p className="mt-[0.7rem] lg:mt-[1rem] w-full lg:w-[60%]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem blanditiis adipisicing quam ad quas, consectetur enim doloremque adipisicing fugiat itaque cumque veritatis.
+                </p>
+            </div>
+
+            {/* Map */}
+            <div className="
+                mt-[3rem]
+                h-[55vh]
+            ">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7367.929910678097!2d88.437142!3d22.580414!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0275a6ec25bcf9%3A0xb5d0d60668ec6adf!2sMartin%20Burn%20Business%20Park!5e0!3m2!1sen!2sin!4v1784369188703!5m2!1sen!2sin" width="100%" height="100%" allowFullScreen loading="lazy" referrerPolicy="strict-origin-when-cross-origin"></iframe>
+            </div>
+
+        </section>
+    )
+}
+
+export default OfficeLocation
