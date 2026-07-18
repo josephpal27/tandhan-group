5fe74594fc74b0e10da6b685014c2040f3ec3228
Joseph Pal <josephpal27@gmail.com>
2026-07-18 13:30:45 +0530

    Contact Page Init

---
+++ src/app/contact/page.js
@@ -0,0 +1,18 @@
+import HeroBanner from "@/components/HeroBanner";
+
+export const metadata = {
+    title: "Contact Us - Tandhan Group",
+    description: "",
+};
+
+export default function Contact() {
+    return (
+        <>
+            <HeroBanner
+                image="/images/banners/media-banner.avif"
+                title="Contact Us"
+                desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio numquam nihil voluptatibus laboriosam reiciendis veniam deserunt, quas voluptatem molestias deleniti vitae exercitationem."
+            />
+        </>
+    );
+}
