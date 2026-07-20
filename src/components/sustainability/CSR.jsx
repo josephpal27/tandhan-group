b55c3cd3261aeefdff56fb86058328acd53e52ee
Joseph Pal <josephpal27@gmail.com>
2026-07-20 13:23:49 +0530

    Sustainablity Page Mobile View Updated

--- src/components/sustainability/CSR.jsx
+++ src/components/sustainability/CSR.jsx
@@ -3,7 +3,7 @@ const CSR = () => {
     return (
         <section className="
             bg-[url('/images/sustainability/csr-banner.avif')] bg-cover bg-center bg-no-repeat text-white
-            pt-[4rem] sm:pt-[5rem] lg:pt-[9rem] xl:pt-[9.5rem] 2xl:pt-[10rem]
+            pt-[3rem] sm:pt-[4rem] lg:pt-[9rem] xl:pt-[9.5rem] 2xl:pt-[10rem]
             pb-[1rem] sm:pb-[1.5rem] lg:pb-[2rem] xl:pb-[2.5rem] 2xl:pb-[3rem]
 
             mt-[5.5rem]
