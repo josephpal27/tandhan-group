359f2ca072b5c0932c25eb89b36576758956effe
Joseph Pal <josephpal27@gmail.com>
2026-07-23 13:37:10 +0530

    Gallery Images Compressed to AVIF

--- src/data/sectorsGalleryData.js
+++ src/data/sectorsGalleryData.js
@@ -27,13 +27,13 @@ export const sectorsGalleryData = {
     textiles: {
         desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident magni.",
         images: [
-            { id: 1, image: "/images/sectors/textiles/gallery/1.png", },
-            { id: 2, image: "/images/sectors/textiles/gallery/2.png", },
-            { id: 3, image: "/images/sectors/textiles/gallery/3.png", },
-            { id: 4, image: "/images/sectors/textiles/gallery/4.png", },
-            { id: 5, image: "/images/sectors/textiles/gallery/5.png", },
-            { id: 6, image: "/images/sectors/textiles/gallery/6.png", },
-            { id: 6, image: "/images/sectors/textiles/gallery/7.png", },
+            { id: 1, image: "/images/sectors/textiles/gallery/1.avif", },
+            { id: 2, image: "/images/sectors/textiles/gallery/2.avif", },
+            { id: 3, image: "/images/sectors/textiles/gallery/3.avif", },
+            { id: 4, image: "/images/sectors/textiles/gallery/4.avif", },
+            { id: 5, image: "/images/sectors/textiles/gallery/5.avif", },
+            { id: 6, image: "/images/sectors/textiles/gallery/6.avif", },
+            { id: 6, image: "/images/sectors/textiles/gallery/7.avif", },
         ]
     },
 
@@ -45,7 +45,7 @@ export const sectorsGalleryData = {
             { id: 3, image: "/images/sectors/hospitality/gallery/3.avif", },
             { id: 4, image: "/images/sectors/hospitality/gallery/4.avif", },
             { id: 5, image: "/images/sectors/hospitality/gallery/5.avif", },
-            // { id: 6, image: "/images/sectors/hospitality/gallery/6.avif", },
+            { id: 6, image: "/images/sectors/hospitality/gallery/6.avif", },
         ]
     },
 }
