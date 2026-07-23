a57325bc210f772b4159c0d0975a5d1574fb1837
Joseph Pal <josephpal27@gmail.com>
2026-07-23 13:28:28 +0530

    Product Images Updated

--- src/data/sectorsGalleryData.js
+++ src/data/sectorsGalleryData.js
@@ -2,13 +2,13 @@ export const sectorsGalleryData = {
     protective: {
         desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident magni.",
         images: [
-            { id: 1, image: "/images/sectors/protective-solutions/gallery/1.png", },
-            { id: 2, image: "/images/sectors/protective-solutions/gallery/2.png", },
-            { id: 3, image: "/images/sectors/protective-solutions/gallery/3.png", },
-            { id: 4, image: "/images/sectors/protective-solutions/gallery/4.png", },
-            { id: 5, image: "/images/sectors/protective-solutions/gallery/5.png", },
-            { id: 6, image: "/images/sectors/protective-solutions/gallery/6.png", },
-            { id: 7, image: "/images/sectors/protective-solutions/gallery/7.png", },
+            { id: 1, image: "/images/sectors/protective-solutions/gallery/1.avif", },
+            { id: 2, image: "/images/sectors/protective-solutions/gallery/2.avif", },
+            { id: 3, image: "/images/sectors/protective-solutions/gallery/3.avif", },
+            { id: 4, image: "/images/sectors/protective-solutions/gallery/4.avif", },
+            { id: 5, image: "/images/sectors/protective-solutions/gallery/5.avif", },
+            { id: 6, image: "/images/sectors/protective-solutions/gallery/6.avif", },
+            { id: 7, image: "/images/sectors/protective-solutions/gallery/7.avif", },
         ]
     },
 
@@ -40,12 +40,12 @@ export const sectorsGalleryData = {
     hospitality: {
         desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, temporibus exercitationem voluptate odio dolorem sit enim provident magni.",
         images: [
-            { id: 1, image: "/images/sectors/hospitality/gallery/1.png", },
-            { id: 2, image: "/images/sectors/hospitality/gallery/2.png", },
-            { id: 3, image: "/images/sectors/hospitality/gallery/3.png", },
-            { id: 4, image: "/images/sectors/hospitality/gallery/4.png", },
-            { id: 5, image: "/images/sectors/hospitality/gallery/5.png", },
-            // { id: 6, image: "/images/sectors/hospitality/gallery/6.png", },
+            { id: 1, image: "/images/sectors/hospitality/gallery/1.avif", },
+            { id: 2, image: "/images/sectors/hospitality/gallery/2.avif", },
+            { id: 3, image: "/images/sectors/hospitality/gallery/3.avif", },
+            { id: 4, image: "/images/sectors/hospitality/gallery/4.avif", },
+            { id: 5, image: "/images/sectors/hospitality/gallery/5.avif", },
+            // { id: 6, image: "/images/sectors/hospitality/gallery/6.avif", },
         ]
     },
 }
