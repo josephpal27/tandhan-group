775e51f762af31b5e2bda6fa15bf785ee602b67a
Joseph Pal <josephpal27@gmail.com>
2026-07-24 17:26:48 +0530

    Country Brands Updated

--- src/data/globalPresenceData.js
+++ src/data/globalPresenceData.js
@@ -1,228 +1,8 @@
-// export const globalPresenceData = [
-//     {
-//         id: 1,
-//         country: "India",
-//         image: "/images/presence/india.avif",
-//         industryImage: "/images/presence//industry/india.avif",
-//         brands: [
-//             {
-//                 brand: "Tandhan Polyplast Ltd.",
-//                 address: "JL-15, Kashyabpur, Uluberia-Amta Rd, Kulgachia, Howrah-711303, West Bengal, India" 
-//             },
-//             {
-//                 brand: "Tandhan Denim",
-//                 address: "Martin Burn Business Park, 18th floor, Plot 3, Salt Lake Bypass, BP Block, Sector V, Bidhannagar, Kolkata, West Bengal 700091"
-//             },
-//         ],
-//     },
-//     {
-//         id: 2,
-//         country: "Bangladesh",
-//         image: "/images/presence/bangladesh.avif",
-//         industryImage: "/images/presence/industry/bangladesh.avif",
-//         brands: [
-//             {
-//                 brand: "Dhaka Power Traders",
-//                 address: "SKS Tower, Level-4, Lift No-14, 7, VIP Road, Mohakhali, Dhaka-1206, Bangladesh" 
-//             },
-//         ],
-//     },
-//     {
-//         id: 3,
-//         country: "Myanmar",
-//         image: "/images/presence/myanmar.avif",
-//         industryImage: "/images/presence/industry/myanmar.avif",
-//         brands: [
-//             {
-//                 brand: "San Taing Kyaw Company Ltd.",
-//                 address: "Room No. 703, 7th Floor, Central Tower, Anawrahtar Road, Bet: 39th & 40th, Yangon, Myanmar (Burma)" 
-//             },
-//         ],
-//     },
-//     {
-//         id: 4,
-//         country: "Dubai",
-//         image: "/images/presence/dubai.avif",
-//         industryImage: "/images/presence/industry/dubai.avif",
-//         brands: [
-//             {
-//                 brand: "Power Global General Trading L.L.C",
-//                 address: "Abdul Al Rahim Mohammad Yousuf Buliding Shop No- 1, Near Naif Park, Satellite Market, Beside Homa Star Hotel, Naif, Deira, PO Box-377064, Dubai, UAE" 
-//             },
-//         ],
-//     },
-//     {
-//         id: 5,
-//         country: "South Africa",
-//         image: "/images/presence/south-africa.avif",
-//         industryImage: "/images/presence/industry/south-africa.avif",
-//         brands: [
-//             {
-//                 brand: "Rainbow Power Solutions",
-//                 address: "Unit F03, Longlake Edge Logistics Park, 25 Daventry Lane, Johannesburg Modderfontein, Sandton-1645, South Africa" 
-//             },
-//         ],
-//     },
-//     {
-//         id: 6,
-//         country: "Kenya",
-//         image: "/images/presence/kenya.avif",
-//         industryImage: "/images/presence/industry/kenya.avif",
-//         brands: [
-//             {
-//                 brand: "Simplified Solar Solutions Ltd.",
-//                 address: "209/8970, Road C. Off Enterprise Road, Industrial Area, Nairobi, Kenya" 
-//             },
-//         ],
-//     },
-//     {
-//         id: 7,
-//         country: "Sudan",
-//         image: "/images/presence/sudan.avif",
-//         industryImage: "/images/presence/industry/sudan.avif",
-//         brands: [
-//             {
-//                 brand: "Global Power Traders",
-//                 address: "Omdurman-Waha district Square 3 - building 51, Portsudan - Koria District Square 3 - Building 320" 
-//             },
-//         ],
-//     },
-//     {
-//         id: 8,
-//         country: "Nigeria",
-//         image: "/images/presence/nigeria.avif",
-//         industryImage: "/images/presence/industry/nigeria.avif",
-//         brands: [
-//             {
-//                 brand: "Sustainable Global Power Tech Ltd.",
-//                 address: "6, Abimbola Street, Isolo Industrial Estate, Lagos State, Nigeria" 
-//             },
-//         ],
-//     },
-//     {
-//         id: 9,
-//         country: "Afghanistan",
-//         image: "/images/presence/afghanistan.avif",
-//         industryImage: "/images/presence/industry/afghanistan.avif",
-//         brands: [
-//             {
-//                 brand: "Ρaktika Ahmadi Trading Co. Ltd.",
-//                 address: "Shop no. 7 Hamidullah Business Centre Kota Sangi, Kabul, Afghanistan" 
-//             },
-//         ],
-//     },
-//     {
-//         id: 10,
-//         country: "Zambia",
-//         image: "/images/presence/zambia.avif",
-//         industryImage: "/images/presence/industry/zambia.avif",
-//         brands: [
-//             {
-//                 brand: "Rainbow Power Solutions",
-//                 address: "Clearwater Office Park, Block L, 2nd floor, Atlas Road, Kempton Park, 1459" 
-//             },
-//         ],
-//     },
-//     {
-//         id: 11,
-//         country: "Tanzania",
-//         image: "/images/presence/tanzania.avif",
-//         industryImage: "/images/presence/industry/tanzania.avif",
-//         brands: [
-//             {
-//                 brand: "Rainbow Power Solutions",
-//                 address: "Clearwater Office Park, Block L, 2nd floor, Atias Road, Kempton Park, 1459" 
-//             },
-//         ],
-//     },
-//     {
-//         id: 12,
-//         country: "DRC",
-//         image: "/images/presence/drc.avif",
-//         industryImage: "/images/presence/industry/drc.avif",
-//         brands: [
-//             {
-//                 brand: "Power Global General Trading L.L.C",
-//                 address: "Abdul Al Rahim Mohammad Yousuf Buliding Shop No-1, Near Naif Park, Satellite Market, Beside Homa Star Hotel, Naif, Deira, PO Box-377064, Dubai, UAE" 
-//             },
-//         ],
-//     },
-//     {
-//         id: 13,
-//         country: "Azerbaijan",
-//         image: "/images/presence/azerbaijan.avif",
-//         industryImage: "/images/presence/industry/azerbaijan.avif",
-//         brands: [
-//             {
-//                 brand: "Power Global General Trading L.L.C",
-//                 address: "Abdul Al Rahim Mohammad Yousuf Buliding Shop No-1, Near Naif Park, Satellite Market, Beside Homa Star Hotel, Naif, Deira, PO Box-377064, Dubai, UAE" 
-//             },
-//         ],
-//     },
-//     {
-//         id: 14,
-//         country: "Ivory Coast",
-//         image: "/images/presence/ivory-coast.avif",
-//         industryImage: "/images/presence/industry/ivory-coast.avif",
-//         brands: [
-//             {
-//                 brand: "Tandhan Sustainable Agro",
-//                 address: "SUSTAINABLE AGRO Vridi Cite' Port, Abidjan, Co'te d' Ivoire 01 BP 12171, ABIDJAN 01" 
-//             },
-//         ],
-//     },
-//     {
-//         id: 15,
-//         country: "Germany",
-//         image: "/images/presence/germany.avif",
-//         industryImage: "/images/presence/industry/germany.avif",
-//         brands: [
-            
-//         ],
-//     },
-//     {
-//         id: 16,
-//         country: "Canada",
-//         image: "/images/presence/canada.avif",
-//         industryImage: "/images/presence/industry/canada.avif",
-//         brands: [
-            
-//         ],
-//     },
-//     {
-//         id: 17,
-//         country: "Brazil",
-//         image: "/images/presence/brazil.avif",
-//         industryImage: "/images/presence/industry/brazil.avif",
-//         brands: [
-            
-//         ],
-//     },
-//     {
-//         id: 18,
-//         country: "Australia",
-//         image: "/images/presence/australia.avif",
-//         industryImage: "/images/presence/industry/australia.avif",
-//         brands: [
-            
-//         ],
-//     },
-// ] 
-
-
-
-
-
-
-
-
-
-
 export const globalPresenceData = [
     {
         id: 1,
         country: "India",
-        image: "/images/presence/india.avif",
+        flag: "/images/presence/flags/india.png",
         industryImage: "/images/presence//industry/india.avif",
         lat: 20.5937,
         lng: 78.9629,
@@ -236,11 +16,12 @@ export const globalPresenceData = [
                 address: "Martin Burn Business Park, 18th floor, Plot 3, Salt Lake Bypass, BP Block, Sector V, Bidhannagar, Kolkata, West Bengal 700091"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 2,
         country: "Bangladesh",
-        image: "/images/presence/bangladesh.avif",
+        flag: "/images/presence/flags/bangladesh.png",
         industryImage: "/images/presence/industry/bangladesh.avif",
         lat: 23.685,
         lng: 90.3563,
@@ -250,11 +31,12 @@ export const globalPresenceData = [
                 address: "SKS Tower, Level-4, Lift No-14, 7, VIP Road, Mohakhali, Dhaka-1206, Bangladesh"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png",],
     },
     {
         id: 3,
         country: "Myanmar",
-        image: "/images/presence/myanmar.avif",
+        flag: "/images/presence/flags/myanmar.png",
         industryImage: "/images/presence/industry/myanmar.avif",
         lat: 21.9162,
         lng: 95.956,
@@ -264,11 +46,12 @@ export const globalPresenceData = [
                 address: "Room No. 703, 7th Floor, Central Tower, Anawrahtar Road, Bet: 39th & 40th, Yangon, Myanmar (Burma)"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 4,
         country: "Dubai",
-        image: "/images/presence/dubai.avif",
+        flag: "/images/presence/flags/dubai.png",
         industryImage: "/images/presence/industry/dubai.avif",
         lat: 24.7005,
         lng: 55.2708,
@@ -278,11 +61,12 @@ export const globalPresenceData = [
                 address: "Abdul Al Rahim Mohammad Yousuf Buliding Shop No- 1, Near Naif Park, Satellite Market, Beside Homa Star Hotel, Naif, Deira, PO Box-377064, Dubai, UAE"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png",],
     },
     {
         id: 5,
         country: "South Africa",
-        image: "/images/presence/south-africa.avif",
+        flag: "/images/presence/flags/south-africa.png",
         industryImage: "/images/presence/industry/south-africa.avif",
         lat: -30.5595,
         lng: 22.9375,
@@ -292,11 +76,12 @@ export const globalPresenceData = [
                 address: "Unit F03, Longlake Edge Logistics Park, 25 Daventry Lane, Johannesburg Modderfontein, Sandton-1645, South Africa"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 6,
         country: "Kenya",
-        image: "/images/presence/kenya.avif",
+        flag: "/images/presence/flags/kenya.png",
         industryImage: "/images/presence/industry/kenya.avif",
         lat: -1.2921,
         lng: 36.8219,
@@ -306,11 +91,12 @@ export const globalPresenceData = [
                 address: "209/8970, Road C. Off Enterprise Road, Industrial Area, Nairobi, Kenya"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 7,
         country: "Sudan",
-        image: "/images/presence/sudan.avif",
+        flag: "/images/presence/flags/sudan.png",
         industryImage: "/images/presence/industry/sudan.avif",
         lat: 12.8628,
         lng: 30.2176,
@@ -320,11 +106,12 @@ export const globalPresenceData = [
                 address: "Omdurman-Waha district Square 3 - building 51, Portsudan - Koria District Square 3 - Building 320"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 8,
         country: "Nigeria",
-        image: "/images/presence/nigeria.avif",
+        flag: "/images/presence/flags/nigeria.png",
         industryImage: "/images/presence/industry/nigeria.avif",
         lat: 9.082,
         lng: 8.6753,
@@ -334,11 +121,12 @@ export const globalPresenceData = [
                 address: "6, Abimbola Street, Isolo Industrial Estate, Lagos State, Nigeria"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 9,
         country: "Afghanistan",
-        image: "/images/presence/afghanistan.avif",
+        flag: "/images/presence/flags/afghanistan.png",
         industryImage: "/images/presence/industry/afghanistan.avif",
         lat: 33.9391,
         lng: 67.71,
@@ -348,11 +136,12 @@ export const globalPresenceData = [
                 address: "Shop no. 7 Hamidullah Business Centre Kota Sangi, Kabul, Afghanistan"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 10,
         country: "Zambia",
-        image: "/images/presence/zambia.avif",
+        flag: "/images/presence/flags/zambia.png",
         industryImage: "/images/presence/industry/zambia.avif",
         lat: -13.1339,
         lng: 27.8493,
@@ -362,11 +151,12 @@ export const globalPresenceData = [
                 address: "Clearwater Office Park, Block L, 2nd floor, Atlas Road, Kempton Park, 1459"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 11,
         country: "Tanzania",
-        image: "/images/presence/tanzania.avif",
+        flag: "/images/presence/flags/tanzania.png",
         industryImage: "/images/presence/industry/tanzania.avif",
         lat: -6.369,
         lng: 34.8888,
@@ -376,11 +166,12 @@ export const globalPresenceData = [
                 address: "Clearwater Office Park, Block L, 2nd floor, Atias Road, Kempton Park, 1459"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 12,
         country: "DRC",
-        image: "/images/presence/drc.avif",
+        flag: "/images/presence/flags/drc.png",
         industryImage: "/images/presence/industry/drc.avif",
         lat: -4.0383,
         lng: 21.7587,
@@ -390,11 +181,12 @@ export const globalPresenceData = [
                 address: "Abdul Al Rahim Mohammad Yousuf Buliding Shop No-1, Near Naif Park, Satellite Market, Beside Homa Star Hotel, Naif, Deira, PO Box-377064, Dubai, UAE"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 13,
         country: "Azerbaijan",
-        image: "/images/presence/azerbaijan.avif",
+        flag: "/images/presence/flags/azerbaijan.png",
         industryImage: "/images/presence/industry/azerbaijan.avif",
         lat: 40.1431,
         lng: 47.5769,
@@ -404,11 +196,12 @@ export const globalPresenceData = [
                 address: "Abdul Al Rahim Mohammad Yousuf Buliding Shop No-1, Near Naif Park, Satellite Market, Beside Homa Star Hotel, Naif, Deira, PO Box-377064, Dubai, UAE"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 14,
         country: "Ivory Coast",
-        image: "/images/presence/ivory-coast.avif",
+        flag: "/images/presence/flags/ivory-coast.png",
         industryImage: "/images/presence/industry/ivory-coast.avif",
         lat: 7.54,
         lng: -5.5471,
@@ -418,41 +211,46 @@ export const globalPresenceData = [
                 address: "SUSTAINABLE AGRO Vridi Cite' Port, Abidjan, Co'te d' Ivoire 01 BP 12171, ABIDJAN 01"
             },
         ],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 15,
         country: "Germany",
-        image: "/images/presence/germany.avif",
+        flag: "/images/presence/flags/germany.png",
         industryImage: "/images/presence/industry/germany.avif",
         lat: 51.1657,
         lng: 10.4515,
         brands: [],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 16,
         country: "Canada",
-        image: "/images/presence/canada.avif",
+        flag: "/images/presence/flags/canada.png",
         industryImage: "/images/presence/industry/canada.avif",
         lat: 56.1304,
         lng: -106.3468,
         brands: [],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 17,
         country: "Brazil",
-        image: "/images/presence/brazil.avif",
+        flag: "/images/presence/flags/brazil.png",
         industryImage: "/images/presence/industry/brazil.avif",
         lat: -14.235,
         lng: -51.9253,
         brands: [],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
     {
         id: 18,
         country: "Australia",
-        image: "/images/presence/australia.avif",
+        flag: "/images/presence/flags/australia.png",
         industryImage: "/images/presence/industry/australia.avif",
         lat: -25.2744,
         lng: 133.7751,
         brands: [],
+        logo: ["/images/logo/tandhan-polyplast.png", "/images/logo/tandhan-denim.png", "/images/logo/tandhan-power.png"],
     },
 ]
