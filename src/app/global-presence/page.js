4463fd8cd18fd33c196963c05271fcf27ac3a5db
Joseph Pal <josephpal27@gmail.com>
2026-07-24 12:19:07 +0530

    Global Presence Page Init

--- src/app/global-presence/page.js
+++ src/app/global-presence/page.js
@@ -1,3 +1,4 @@
+import HeroBanner from "@/components/HeroBanner";
 
 export const metadata = {
   title: "Global Presence - Tandhan Group",
@@ -7,7 +8,11 @@ export const metadata = {
 export default function GlobalPresence() {
   return (
     <>
-      
+      <HeroBanner
+        image="/images/banners/presence-banner.png"
+        title="Global Presence"
+        desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio numquam nihil voluptatibus laboriosam reiciendis veniam deserunt, quas voluptatem molestias deleniti vitae exercitationem."
+      />
     </>
   );
 }
