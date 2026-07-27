f9f15ddf609ced9801feaa945548839a50e497c2
Joseph Pal <josephpal27@gmail.com>
2026-07-27 17:54:30 +0530

    Team Page Updated

--- src/components/team/TeamList.jsx
+++ src/components/team/TeamList.jsx
@@ -2,7 +2,10 @@ import {teamData} from "@/data/teamData";
 
 const TeamList = () => {
     return (
-        <section className="pt-[2rem] sm:pt-[3rem] lg:pt-[4rem] xl:pt-[4.5rem] 2xl:pt-[5rem]">
+        <section className="
+            pt-[2rem] sm:pt-[3rem] lg:pt-[4rem] xl:pt-[4.5rem] 2xl:pt-[5rem]
+            pb-[1rem] sm:pb-[1.5rem] lg:pb-[2rem] xl:pb-[2.5rem] 2xl:pb-[3rem]
+        ">
             <h6>
                 Our Leaders
             </h6>
@@ -11,11 +14,28 @@ const TeamList = () => {
             </p>
 
             {/* Row */}
-            <div className="flex flex-wrap mt-[3rem]">
+            <div className="flex flex-wrap gap-[2%] mt-[3rem]">
                 {teamData.map((team, index) => {
                     return(
-                        <div key={index} className="w-[24%]">
-                            <img src="" alt={team.name} />
+                        <div key={index} className="
+                            w-[23.5%] cursor-pointer group 
+                            mb-[3rem]
+                        ">
+                            <div className="overflow-hidden">
+                                <img src={team.image} alt={team.name} loading="lazy" className="w-full group-hover:scale-[1.05] transition duration-300" />
+                            </div>
+                            <div>
+                                <span className="
+                                    block mt-[1rem] text-[1.2rem] sm:text-[1.25rem] lg:text-[1.3rem] xl:text-[1.4rem] 2xl:text-[1.5rem] font-semibold
+                                ">
+                                    {team.name}
+                                </span>
+                                <p className="
+                                    text-[0.9rem] sm:text-[1rem] lg:text-[0.9rem] xl:text-[1rem] 2xl:text-[1.1rem]
+                                ">
+                                    {team.position}
+                                </p>
+                            </div>
                         </div>
                     )
                 })}
