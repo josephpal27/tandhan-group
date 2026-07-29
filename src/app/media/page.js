cfedf379c5cad658d1617a49cb6a6b7055b9397c
Joseph Pal <josephpal27@gmail.com>
2026-07-29 13:18:19 +0530

    Media Page Updated

--- src/app/media/page.js
+++ src/app/media/page.js
@@ -1,6 +1,5 @@
 import HeroBanner from "@/components/HeroBanner";
 import MediaListTabbing from "@/components/media/MediaListTabbing";
-// import RelatedBlogs from "@/components/media/RelatedBlogs";
 
 export const metadata = {
     title: "Media - Tandhan Group",
@@ -16,7 +15,6 @@ export default function Media() {
                 desc="Stay connected with the latest news, milestones, achievements, and stories that reflect Tandhan Group's journey of growth, innovation, and industry leadership."
             />
             <MediaListTabbing />
-            {/* <RelatedBlogs /> */}
         </>
     );
 }
