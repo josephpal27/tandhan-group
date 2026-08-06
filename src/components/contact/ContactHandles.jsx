9cd4b2d9506b80b8891c19e9894a84371af32491
Joseph Pal <josephpal27@gmail.com>
2026-08-06 11:47:22 +0530

    Contact Details Updated

--- src/components/contact/ContactHandles.jsx
+++ src/components/contact/ContactHandles.jsx
@@ -2,11 +2,11 @@
 import { FaFacebookF, FaXTwitter, FaInstagram, FaLinkedinIn, FaYoutube } from "react-icons/fa6";
 
 const handleIcons = [
-    {   id: 1, icon: <FaFacebookF /> },
-    {   id: 2, icon: <FaXTwitter /> },
-    {   id: 3, icon: <FaInstagram /> },
-    {   id: 4, icon: <FaLinkedinIn /> },
-    {   id: 5, icon: <FaYoutube /> },
+    {   id: 1, icon: <FaFacebookF />, url: "https://www.facebook.com/tandhan.official" },
+    {   id: 2, icon: <FaXTwitter />, url: "https://x.com/Tandhanofficial" },
+    {   id: 3, icon: <FaInstagram />, url: "https://www.instagram.com/tandhan.official/" },
+    {   id: 4, icon: <FaLinkedinIn />, url: "https://www.linkedin.com/company/tandhan-group" },
+    {   id: 5, icon: <FaYoutube />, url: "https://www.youtube.com/channel/UCBN4sdODzzdx32Yw8anJ_sQ" },
 ]
 
 const ContactHandles = () => {
@@ -24,8 +24,8 @@ const ContactHandles = () => {
                 className="w-[60%] lg:w-[250px]" 
             />
 
-            <p className="mt-[0.7rem] lg:mt-[1rem] w-full lg:w-[60%]">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique consectetur amet voluptates amet saepe quo at optio beatae quod eaque quam totam aspernatur.
+            <p className="mt-[0.7rem] lg:mt-[1rem] w-full lg:w-[55%]">
+                For a closer look into the heart of Tandhan, and to journey with us through every innovation, project, and milestone, connect with us on our social platforms.
             </p>
 
             <div className="mt-[1.7rem] lg:mt-[2rem] flex justify-center gap-[1rem]">
@@ -37,7 +37,7 @@ const ContactHandles = () => {
                             text-[1.2rem]
                             rounded-full bg-[#2d2d2d] hover:bg-primary text-white flex justify-center items-center transition duration-300 hover:scale-110
                         ">
-                            <a href="#" target="_blank">
+                            <a href={item.url} target="_blank" rel="noopener noreferrer">
                                 {item.icon}
                             </a>
                         </div>
