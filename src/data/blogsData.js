9a2a7110ef8206fa35757aa029d0c22f0d8055e5
Joseph Pal <josephpal27@gmail.com>
2026-09-04 18:09:49 +0530

    Media Updated

--- src/data/blogsData.js
+++ src/data/blogsData.js
@@ -3,27 +3,27 @@ export const blogsData = [
         id: 1,
         slug: "sustainable-manufacturing",
         title: "Sustainable Manufacturing",
-        // desc: "Lorem ipsum dolor sit amet, consectetur adipisi elit. Distinctio numquam.",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisi elit. Distinctio numquam.",
         date: "17th Jul, 2026",
         image: "/images/media/demo.avif",
-        // content: `
-        //     <em>
-        //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem. Sint qui consequatur vero rerum omnis iusto.
-        //     </em>
+        content: `
+            <em>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem. Sint qui consequatur vero rerum omnis iusto.
+            </em>
 
-        //     <p>
-        //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem. Sint qui consequatur vero rerum omnis iusto, delectus et dolorum consectetur adipisicing elit aliquam consectetur. Lorem ipsum dolor sint qui consequatur sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem. Sint qui consequatur vero rerum omnis iusto, delectus et dolorum aliquam consectetur.
-        //     </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem. Sint qui consequatur vero rerum omnis iusto, delectus et dolorum consectetur adipisicing elit aliquam consectetur. Lorem ipsum dolor sint qui consequatur sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem. Sint qui consequatur vero rerum omnis iusto, delectus et dolorum aliquam consectetur.
+            </p>
 
-        //     <p>
-        //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem. Sint qui consequatur vero rerum omnis iusto, delectus et dolorum aliquam consectetur.
-        //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem.
-        //     </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem. Sint qui consequatur vero rerum omnis iusto, delectus et dolorum aliquam consectetur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem.
+            </p>
 
-        //     <p>
-        //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem. Sint qui consequatur vero rerum omnis iusto, delectus et dolorum aliquam consectetur.
-        //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem. Sint qui consequatur vero rerum omnis iusto.
-        //     </p>
-        // `,
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem. Sint qui consequatur vero rerum omnis iusto, delectus et dolorum aliquam consectetur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, veritatis illo. Unde illo, reprehenderit labore nostrum odit autem. Sint qui consequatur vero rerum omnis iusto.
+            </p>
+        `,
     },
 ];
